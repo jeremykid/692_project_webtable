4a6e8c85434131cd891b6a8aa178ce527386b380
weijiesun <weijie2@ualberta.ca>
2020-10-31 20:34:30 -0600

    Improve timeout exception

--- helpers/web_table.py
+++ helpers/web_table.py
@@ -3,26 +3,32 @@ import sys
 import subprocess
 import urllib.request
 import pandas as pd
+import time
 
 def extractor(site):
     '''Extract tables from a single or a list of urls or html filenames passed.'''
     header = {"User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.75 Safari/537.36",
       "X-Requested-With": "XMLHttpRequest"}
-    try:
-
-        if len(site.split('//'))>1:
-            fname = site.split('//')[1]
-        else:
-            fname = site
-            site = 'http://'+site
-
-#     print('Extracting tables from: ' + site)
     
+    try:
+        time_out = time.process_time() + 120 # 2 minutes timeout
 
-        req=urllib.request.Request(site, headers=header)
-        content = urllib.request.urlopen(req).read()
-        df1= pd.read_html(content)
-        return df1, ''
+        while time.process_time() <= time_out:
+            if len(site.split('//'))>1:
+                fname = site.split('//')[1]
+            else:
+                fname = site
+                site = 'http://'+site
+
+    #     print('Extracting tables from: ' + site)
+
+
+            req=urllib.request.Request(site, headers=header)
+            content = urllib.request.urlopen(req).read()
+            df1= pd.read_html(content)
+            return df1, ''
+        else:
+            raise Exception('timeout')
     except Exception as e:
         df1=[]
         return df1, e
